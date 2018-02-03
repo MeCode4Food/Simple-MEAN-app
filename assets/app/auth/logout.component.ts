8ccc79b6c0d4db7aea97a2ea8262e5734b3a9c05
MeCode4Food <cheekhuin.loong@gmail.com>
2018-02-03 19:09:03 +0800

    up to lecture 56

---
+++ assets/app/auth/logout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core/src/metadata/directives";
+
+@Component({
+    selector: 'app-logout',
+    template: `
+        <div class="col-md-8 col-md-offset-2">
+        </div>
+    `
+})
+
+export class LogoutComponent {
+
+}
--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { MessageService } from "./message.service";
 import { Message } from "./message.model";
+import { NgForm } from "@angular/forms/src/directives/ng_form";
 
 @Component({
     selector: 'app-message-input',
@@ -12,8 +13,9 @@ export class MessageInputComponent{
         
     }
 
-    onSave(value:string) {
-        const message: Message = new Message(value, 'CK');
+    onSubmit(form: NgForm) {
+        const message: Message = new Message(form.value.content, 'CK');
         this.messageService.addMessage(message);
+        form.resetForm();
     }
 }
